99ae5a2c2d7812f0d7ec27c212b4b0105526cf26
Liza Grossman <lisok.sam@gmail.com>
2023-01-29 20:27:10 +0200

    work with UI

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func greetingButtonDidTapp() {
+        greetingLabel.isHidden.toggle()
+    }
+    
 }
 
